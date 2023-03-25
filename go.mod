9449bbc2ea44b3e23f201e6d22d45844832c03b1
moabukar <mo-abukar10@hotmail.com>
2023-03-25 03:44:39 +0000

    build simple start/stop service, makefile + Dockerfile

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/moabukar/go-labs-tools
 
 go 1.19
-
-require github.com/ardanlabs/conf v1.5.0
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"github.com/ardanlabs/conf"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+var build = "develop"
+
 func main() {
-	conf.New()
+
+	log.Println("starting service", build)
+	defer log.Println("service ended")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stopping service", build)
 }
